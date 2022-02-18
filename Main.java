192a85ca7189e43285af123047dec70853be29fd
joshgould24 <57639466+joshgould24@users.noreply.github.com>
2022-02-18 17:02:38 +0000

    First Commit!
    very exciting.
    **insert useful informationa about commit here**

---
+++ Main.java
@@ -0,0 +1,22 @@
+package mvcexample;
+
+/**
+ *
+ * @author savi
+ */
+// Launcher for application
+
+public class Main {
+   
+    public static void main(String args[]) {
+     
+        Model model = new Model();               // Create shared database
+        Controller1 c1 = new Controller1(model, 40, 40); // Create first controller
+        Controller2 c2 = new Controller2(model, "v1", 40,200); // Create second controller
+        Controller2 c22 = new Controller2(model, "v2", 405,200); // Create second controller
+        Controller2 c23 = new Controller2(model, "v3", 40,350); // Create second controller
+        Controller2 c24 = new Controller2(model, "v4", 405,350); // Create second controller
+        
+    } // main
+  
+} // Main
